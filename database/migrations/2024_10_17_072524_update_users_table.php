f71d66b347c966a85138f70baa7908625f35b1ea
Ézéchiel Johnstone <ezechiel.johnstone@gmail.com>
2024-10-17 09:38:49 +0200

    Migrated plants table

---
+++ database/migrations/2024_10_17_072524_update_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('first_name')->after('name'); // Adds the 'first_name' column after 'name'
+            $table->string('profile_picture')->nullable()->after('email'); // Adds 'profile_picture' column after 'email'
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['first_name', 'profile_picture']); // Removes the columns if the migration is rolled back
+        });
+    } //
+};
---
+++ database/migrations/2024_10_17_073438_create_plants_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plants', function (Blueprint $table) {
+            $table->id('plant_id');
+            $table->tinyText('plant_common_name');
+            $table->tinyText('plant_scientific_name');
+            $table->text('plant_description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('plants');
+    }
+};
